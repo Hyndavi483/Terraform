1326b9706c93e2b64a806820b7eb0c958c8362af
Hyndavi483 <nhyndavi483@gmail.com>
2025-01-26 09:53:02 +0530

    Create main.tf

---
+++ Provisioners/main.tf
@@ -0,0 +1,68 @@
+provider "aws" {
+  region = "ap-south-1"
+}
+resource "aws_key_pair" "example" {
+  key_name   = "hynduubu"  # Replace with your desired key name
+  public_key = file("~/.ssh/id_rsa.pub")  # Replace with the path to your public key file
+}
+resource "aws_security_group" "webSg" {
+  name   = "web"
+
+  ingress {
+    description = "HTTP from VPC"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  ingress {
+    description = "SSH"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "Web-sg"
+  }
+}
+resource "aws_instance" "test" {
+  ami = "ami-00bb6a80f01f03502"
+  key_name = "hynduubu"
+  instance_type = "t2.micro"
+  vpc_security_group_ids = [aws_security_group.webSg.id]
+
+ connection {
+    type        = "ssh"
+    user        = "ubuntu"  # Replace with the appropriate username for your EC2 instance
+    private_key = file("~/.ssh/id_rsa")  # Replace with the path to your private key
+    host        = self.public_ip
+  }
+
+
+
+# File provisioner to copy a file from local to the remote EC2 instance
+  provisioner "file" {
+    source      = "app.py"  # Replace with the path to your local file
+    destination = "/home/ubuntu/app.py"  # Replace with the path on the remote instance
+  }
+
+  provisioner "remote-exec" {
+    inline = [
+      "echo 'Hello from the remote instance'",
+      "sudo apt update -y",  # Update package lists (for ubuntu)
+      "sudo apt install python3-flask",
+      "cd /home/ubuntu",
+      "sudo apt install python3-flask",
+      "sudo python3 app.py &",
+    ]
+  }
+}
